952c92a6eaf0867621629c013b793ba427af67f5
Joohong-kim <zzangtra@gmail.com>
2023-05-11 16:40:56 +0900

    웹기능사실기

---
+++ js/basic.js
@@ -0,0 +1,17 @@
+// $() 는 주어 부르기 / $() = jquery 선택자라고 부름
+
+// $("#detail a").click(function(){})
+
+// $("#slider li").click(function(){})
+// $("#slider li:first-child").click(function(){})
+// $("#slider li:nth-child(2)").click(function(){})
+
+// // 아이디 slider 첫번째 li안의 자식 a태그를 클릭하면 실행하라
+// $("#slider li:nth-child(1) > a").click(function(){})
+
+// // var num = 0; -> 0이 초기값이다.
+// var num = 0;
+// num = 1;
+// $("#slider li").eq(num).find(">a").click(function(){})
+
+$(document).ready(function () {});
